418c320097859567738442a0e9d3b228f7c4db76
Stellar <stellarcryptos@gmail.com>
2024-08-13 02:26:17 +0100

    new

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+
+// JAVASCRIPT Numbers
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// JAVASCRIPT STRINGS
+
+let color = "Yellow";
+let lastName = "Stellar the Developer"
+
+console.log(color);
+console.log(lastName);
+
+// Boolean in Javascript
+
+let a = 5;
+let b = 5;
+let c = 7;
+
+console.log(a==b);
+console.log(a==c);
+
+// JAVASCRIPT ARRAYS
+
+let namesOfStudents = [
+    "Stellar", "Eniola", "Peace", "Celestinah"
+];
+
+
+// Javascript objects
+
+const person = {
+     firstName : "Stellarbaby", 
+     lastName : "Heritage",
+     age : 25,
+     eyeColor : "Black",
+     school : "Osun State University"
+}
+
+console.log(person);
+
