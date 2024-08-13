d4bfd726397c838c0c1cd0da44b5c78972f3d4aa
Stellar <stellarcryptos@gmail.com>
2024-08-13 06:47:41 +0100

    new

--- index.js
+++ index.js
@@ -28,12 +28,28 @@ let c = 7;
 console.log(a==b);
 console.log(a==c);
 
+// ANOTHER METHOD IN BOOLEAN
+  
+var o = 5, p = 6, r = 5;
+console.log(o>p);
+console.log(r==o);
+
+
 // JAVASCRIPT ARRAYS
 
 let namesOfStudents = [
     "Stellar", "Eniola", "Peace", "Celestinah"
 ];
+console.log(namesOfStudents);
 
+let arraysOfCars = [
+    "Volvo", "Benz", "Toyota"
+];
+console.log(arraysOfCars);
+
+
+let pets = ["cats", "dogs", "hens", "cows"];
+console.log(pets); 
 
 // Javascript objects
 
@@ -47,6 +63,14 @@ const person = {
 
 console.log(person);
 
+const robot = {
+    names : "AI product",
+    uses : "To ease the wordload of people",
+    age : 1,
+}
+console.log(robot);
+
+
 let d, e, f, myName, classOf, position;
 d = 5; e = 6; f = 7; myName = "Oluwatobiloba Stellar"; classOf = 24; position = "Law graduate";
 
@@ -57,3 +81,20 @@ console.log(myName);
 console.log(classOf);
 console.log(position);
 
+// FUNCIONS IN JS
+
+let add = function(a, b) {
+    return "a+b";
+}
+console.log(add());
+
+function greeting() {
+    return "Hello world";
+}
+console.log(greeting());
+
+function myFunNames(names) {
+return "My names are Stellar Heritage Oluwatobiloba and i am a developer."
+}
+
+console.log(myFunNames());
